fa63e0f98dcaffccd19e1bf9cf27e1cc15cc99b4
wuhao <emmettng@gmail.com>
2019-12-02 16:18:04 +0800

    1. finished complex reader example   1.1 revise this staging example 2. start adding more to state simple example

--- src/Monadic/ReaderState/ReaderForHuman.hs
+++ src/Monadic/ReaderState/ReaderForHuman.hs
@@ -1,20 +1,18 @@
-module Monadic.ReaderState.ForHuman where
+{-# LANGUAGE RankNTypes #-}
+
+module Monadic.ReaderState.ReaderForHuman where
 
 -- newtype ReaderT r m a = ReaderT { runReaderT :: r -> m a }
-
 -- Summary
-
 -- Function 'ask' introduce the 'env' informatoin into the Reader Monad
 -- 'local' (withReaderT) alter the 'env' information temporarily.
 -- 'asks' convert a function from 'env' to other type to a Reader Monad.
 -- Usually, one 'ask' will related to one Reader Monad that represent a function depends on 'env' information.
 -- Because there could be several function depends on same 'env'. They can be composed by >>= or >=>.
-
 -- ask
 -- ask :: (Monad m) => ReaderT r m r
 -- ask = ReaderT return
 -- return :: r -> m r
-
 -- local
 -- local
 --     :: (r -> r)         -- ^ The function to modify the environment.
@@ -27,63 +25,58 @@ module Monadic.ReaderState.ForHuman where
 --     -> ReaderT r m a    -- ^ Computation to run in the modified environment.
 --     -> ReaderT r' m a
 -- withReaderT f m = ReaderT $ runReaderT m . f
-
 -- asks
 -- asks :: (Monad m)
 --     => (r -> a)         -- ^ The selector function to apply to the environment.
 --     -> ReaderT r m a
 -- asks f = ReaderT (return . f)
 -- {-# INLINE asks #-}
-
-
 -- | Common usage
 -- 1. use 'ask' to introduce the 'env' into computation. (almost compulsory, asks is rarely being used)
 -- 2. so we could construct functions of type `a -> Reader r b` or `Reader r b`. (compulsory)
 -- 3. 'local' or 'withReaderT' alter enviroment (optional)runreaderT ==> bring out , follow by an Env. (optional)
 -- 4. 'runReader' or 'runreaderT' to unwrapp functions. (compulsory)
 -- 5. Feed the 'env' information (compulsory)
-
 -- | Intuition:
 -- Pass Env/Context/Configuration information through a chain of operations that share the same information.
-
 -- Imports before Example One
-import           Control.Monad.Trans.Reader
-import           GHC.Float
+import Control.Monad.Trans.Reader
+import GHC.Float
 
 -- Imports before Example Two
-import           Control.Monad.IO.Class
-import           Control.Monad    -- kleisli arrow ( >=> )
+import Control.Monad.IO.Class
+import Control.Monad -- kleisli arrow ( >=> )
+
+-- Import before Staging Example 
+import GHC.Float
+import Data.Store
 
 -- The context informaiton is Env
 -- syntax: give me a Env gave you an output
-
-
 -- | Example one ------------->>----------------->>------------->>
 -- Core operations chain
 -- Intuitive Explaination:
 --          CoreInput -> Reader Env CoreOutput
 --          Pass Env down through the core operation chain.
-
 -- core operation 1 :: Int -> Int
 a1 :: Int -> Reader Int Int
 a1 n = do
-    env <- ask           -- get Enviroment information
-    return $ n + env
+  env <- ask -- get Enviroment information
+  return $ n + env
 
 -- core operation 2 :: Into -> Float
 a2 :: Int -> Reader Int Float
 a2 n = do
-    env <- ask          -- get Enviroment information
-    let fn = fromIntegral n
-    return $ fromIntegral env / fn
-
+  env <- ask -- get Enviroment information
+  let fn = fromIntegral n
+  return $ fromIntegral env / fn
 
 -- core operation 3 :: Float -> Double
 a3 :: Float -> Reader Int Double
 a3 f = do
-    env <- ask          -- get Enviroment information
-    let fn = fromIntegral env
-    return $ float2Double $ fn * f
+  env <- ask -- get Enviroment information
+  let fn = fromIntegral env
+  return $ float2Double $ fn * f
 
 -- | A Chain of core operations.
 -- Return a function from `enviroment information` to `result`
@@ -91,56 +84,60 @@ a3 f = do
 -- can be produced.
 chainA :: Int -> Reader Int Double
 chainA n = do
-    t1 <- a1 n          -- core operaiton 1
-    t2 <- a2 t1         -- core operaiton 2
-    a3 t2               -- core operation 3
+  t1 <- a1 n -- core operaiton 1
+  t2 <- a2 t1 -- core operaiton 2
+  a3 t2 -- core operation 3
 
 chainA' :: Int -> Reader Int Double
 chainA' = a1 >=> a2 >=> a3
+
 --
 -- ask :: (Monad m) => ReaderT r m r
 -- ask = ReaderT return
 --
 -- return :: r -> m r
-
 --  let p1 = chainA 10
 --  runReader p1 $ 2
 --  0.3333333432674408
 --  env is 2 and n is 10 so
 --  t1 = 10 + 2 , t2 = 2 / 12,  t3 = 2*2 / 12  =  0.33333333
-
 -- | Example Two: ------------->>----------------->>------------->>
 -- Monad transformer and envrioment information dependence only.
 -- Operations depend only on Env information.
 -- Intuitive Explaination:
 --     Sequence opeartion depends only on Enviroment.
 --
-b1 :: (MonadIO m) => ReaderT String m Int
+b1
+  :: (MonadIO m)
+  => ReaderT String m Int
 b1 = do
-    env <- ask           -- get Enviroment information
-    n   <- liftIO getLine
-    let n1 = read env
-        n2 = read n
-    return $ n1 + n2
+  env <- ask -- get Enviroment information
+  n <- liftIO getLine
+  let n1 = read env
+      n2 = read n
+  return $ n1 + n2
 
 -- core operation 2 :: Into -> Float
-b2 :: (MonadIO m) => ReaderT String m Float
+b2
+  :: (MonadIO m)
+  => ReaderT String m Float
 b2 = do
-    env <- ask          -- get Enviroment information
-    n   <- liftIO getLine
-    let n1 = read env
-        n2 = read n
-    return $ n1 / n2
-
+  env <- ask -- get Enviroment information
+  n <- liftIO getLine
+  let n1 = read env
+      n2 = read n
+  return $ n1 / n2
 
 -- core operation 3 :: Float -> Double
-b3 :: (MonadIO m) => ReaderT String m Double
+b3
+  :: (MonadIO m)
+  => ReaderT String m Double
 b3 = do
-    env <- ask          -- get Enviroment information
-    n   <- liftIO getLine
-    let n1 = read env
-        n2 = read n
-    return $ n1 * n2
+  env <- ask -- get Enviroment information
+  n <- liftIO getLine
+  let n1 = read env
+      n2 = read n
+  return $ n1 * n2
 
 -- | b1 b2 b3, use same Env of type String
 --   Three paraller computations.
@@ -148,12 +145,12 @@ b3 = do
 -- of chain
 tryB :: ReaderT String IO Float
 tryB = do
-    t1 <- b1
-    t2 <- b2
-    t3 <- b3
-    return $ fromIntegral t1 + t2 + double2Float t3
+  t1 <- b1
+  t2 <- b2
+  t3 <- b3
+  return $ fromIntegral t1 + t2 + double2Float t3
 
- -- runReaderT :: ReaderT  --> function :: Env -> Result
+-- runReaderT :: ReaderT  --> function :: Env -> Result
 -- let p = runReaderT tryB
 --  r <- p "10"
 -- 1
@@ -163,9 +160,7 @@ tryB = do
 -- 46.0
 -- 1+10  + 10/2 + 3*10 = 46
 -- env information is 10 but of the input type String
-
 -- | Example Three ------------->>----------------->>------------->>
-
 -- local :: (r -> r) -> ReaderT r m a -> ReaderT r m a
 ---
 -- withReaderT :: (r' -> r) -> ReaderT r m a -> ReaderT r' m a
@@ -178,18 +173,20 @@ tryB = do
 changeEnv :: String -> Float
 changeEnv s = read s + 100
 
-c1 :: (Monad m) => ReaderT Float m Float
+c1
+  :: (Monad m)
+  => ReaderT Float m Float
 c1 = do
-    env <- ask
-    return $ env * 5
+  env <- ask
+  return $ env * 5
 
 tryC :: ReaderT String IO Float
 tryC = do
-    t1  <- b1
-    t2  <- b2
-    tc1 <- withReaderT changeEnv c1
-    t3  <- b3
-    return $ fromIntegral t1 + t2 + double2Float t3 + tc1
+  t1 <- b1
+  t2 <- b2
+  tc1 <- withReaderT changeEnv c1
+  t3 <- b3
+  return $ fromIntegral t1 + t2 + double2Float t3 + tc1
 
 --  let p = runReaderT tryC
 --  r <- p "10"   -- env is "10"
@@ -199,7 +196,6 @@ tryC = do
 --  r
 -- 346.0
 -- 1+10 + 10/2 + 10*3 + 110*5 = 596
-
 -- |
 -- local modify the value of Env
 -- withReaderT is more general, it could modify the type of Env.
@@ -217,7 +213,6 @@ tryC = do
 --     -> ReaderT r' m a
 -- withReaderT f m = ReaderT $ runReaderT m . f
 --
-
 -- | Example Four ------------->>----------------->>------------->>
 -- asks :: (Monad m) => (r - a) -> ReaderT r m a
 -- Intuitive Explaination:
@@ -226,15 +221,14 @@ tryC = do
 simpleFunc :: String -> Float
 simpleFunc s = 1000 + read s
 
-
 tryD :: ReaderT String IO Float
 tryD = do
-    t1  <- b1
-    t2  <- b2
-    tc1 <- withReaderT changeEnv c1
-    t3  <- b3
-    td1 <- asks simpleFunc
-    return $ fromIntegral t1 + t2 + double2Float t3 + tc1 + td1
+  t1 <- b1
+  t2 <- b2
+  tc1 <- withReaderT changeEnv c1
+  t3 <- b3
+  td1 <- asks simpleFunc
+  return $ fromIntegral t1 + t2 + double2Float t3 + tc1 + td1
 
 --  let p = runReaderT tryD
 --  r <- p "10"
@@ -244,3 +238,95 @@ tryD = do
 --  r
 -- 1606.0
 --1+10 + 10/2 +  10*3 + 110 * 5 + 1010 = 1606
+--
+data Env = Env
+  { sf1Env :: String
+  , sf2Env :: Int
+  , sf3Env :: Float
+  , sf4Env :: Double
+  , sf5Env :: Int
+  , serializer :: Serializer
+  , deserializer :: Deserializer
+  }
+
+type StageCore a = ReaderT Env IO a
+
+-- sf1
+--   :: (Monad m)
+--   => ReaderT Env m Int
+sf1 :: StageCore Int
+sf1 = do
+  env <- ask
+  return $ length . sf1Env $ env
+
+sf2 :: Int -> StageCore Float
+sf2 arg2 = do
+  env <- ask
+  return $ fromIntegral $ sf2Env env + arg2
+
+sf3 :: Float -> StageCore Double
+sf3 arg3 = do
+  env <- ask
+  return $ float2Double $ sf3Env env + arg3
+
+sf4 :: Double -> StageCore String
+sf4 arg4 = do
+  env <- ask
+  return $ show $ sf4Env env + arg4
+
+sf5 :: StageCore Int
+sf5 = do
+  env <- ask
+  return $ 100 + sf5Env env
+
+coreChain :: () -> ReaderT Env IO Int
+coreChain = (\_ -> sf1) >=> sf2 >=> sf3 >=> sf4 >=> (\_ -> sf5)
+
+-- | Now we would like to:
+--  1. Serialize the core output of each core functions above on the disk.
+--  2. If relative env information changes, we performe the calculation.
+--  3. If previous calculation is new, we performe this calculation.
+--  4. otherwise, we deserialize what we saved on the disk.
+-- | we need to name each stage 
+type StageName = String
+
+type Core a = (a, Bool)
+
+-- | We use Data.Store to serialize output of each core function 
+-- StageName and Env defines this boolen value 
+checkSerialization :: StageName -> StageCore (Core FilePath)
+checkSerialization = undefined
+
+type Serializer = forall a. (Store a) =>
+                            a -> FilePath -> IO ()
+
+type Deserializer = forall a. (Store a) =>
+                              FilePath -> IO a
+
+-- | This would be make more sense, 
+--  we take a corefunction ( a -> StageCore b) 
+--  and a StageName 
+--  embellish original corefunction output with a Bool information ( a-> StageCore (b,Bool) == (a -> StageCore (Core b))
+--  this bool information is being used for helping following operation to decide whether to do serialization.
+--
+staging
+  :: (Store b)
+  => StageName -> (a -> StageCore b) -> Core a -> StageCore (Core b)
+staging stageName coreFunc (coreInput, preStatus) = do
+  env <- ask
+  let savetoDisk = serializer env
+      readDisk = deserializer env
+  (serializationPath, done) <- checkSerialization stageName
+  if done && preStatus
+    then do
+      ds <- liftIO $ readDisk serializationPath
+      return (ds, True)
+    else do
+      coreOutput <- coreFunc coreInput
+      liftIO $ savetoDisk coreOutput serializationPath
+      return (coreOutput, False)
+
+stageChain =
+  (staging "sf1" (\_ -> sf1)) >=>
+  (staging "sf2" sf2) >=>
+  (staging "sf3" sf3) >=> (staging "sf4" sf4) >=> (staging "sf5" (\_ -> sf5))
